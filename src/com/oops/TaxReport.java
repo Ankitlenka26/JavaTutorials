4ecc55b046d9550bb0dd7066df235e96963e2066
Ankit Lenka <ankitlenka26@gmail.com>
2021-05-05 19:19:13 +0530

    Tightly coupled code

---
+++ src/com/oops/TaxReport.java
@@ -0,0 +1,15 @@
+package com.oops;
+// this is a example of tightly coupled code
+// we are using the class TaxCalculator so we are dependent on this class
+// If our TaxCalculator class changes our report class might be affected
+// ex : what if we have multiple parameter in our taxCalculator , we have to modify all the classes that are dependent on TaxCalculator class
+public class TaxReport {
+    private TaxCalculator calculator;
+    public TaxReport(){
+        calculator = new TaxCalculator(100000);
+    }
+    public void show(){
+        var tax = calculator.calculateTax();
+        System.out.println(tax);
+    }
+}
